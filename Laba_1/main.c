7bfe367f4544b6c854ec58047e3b5e7e32d49b5c
SofiaBudnik <sofabudnik111@gmail.com>
2021-03-23 11:47:39 +0300

    r

--- Laba_1/main.c
+++ Laba_1/main.c
@@ -2,6 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 #include "main.h"
+int vychislen(char a[30], int k)
+{
+    int i=1;
+
+    while(k>0) {
+        itoa(i*i*i, a, 10);
+        k -= strlen(a);
+        i++;
+    }
+    return k += strlen(a);
+
+}
 
 int main ()
 {
