1c8b9ff21b1a5636dbabc95ae24f1a192eb3e7da
SofiaBudnik <sofabudnik111@gmail.com>
2021-03-25 13:45:30 +0300

    gfyj

--- Laba_2/tests.c
+++ Laba_2/tests.c
@@ -2,13 +2,29 @@
 #include <assert.h>
 #include <stdio.h>
 
-char test_vychislen() {
-    assert(vychislen(5)=='6');
-}
 
+int test_storonaA() {
+    assert(storonaA(200,200,300,100)==111);    
+}
+int test_storonaB() {
+    assert(storonaB(300,100,400,200)==111);
+}
+int test_storonaC() {
+    assert(storonaC(400,200,200,200)==111);
+}
+int test_perimeter () {
+   assert(perimeter(111,111,111)==333);
+}
+int test_squared() {
+    assert(squared(111,111,111)==5097);
+}
 #undef main
 int main () {
-    printf ("\n\n%c\n\n", vychislen(5));
+    test_storonaA();
+    test_storonaB();
+    test_storonaC();
+    test_perimeter ();
+    test_squared();
     test_vychislen();
 }
  
