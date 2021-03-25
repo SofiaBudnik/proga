6e2d42add611e5da99e686a976b66fe77442e82f
SofiaBudnik <sofabudnik111@gmail.com>
2021-03-25 10:45:08 +0300

    r

--- Laba_1/tests.c
+++ Laba_1/tests.c
@@ -2,12 +2,12 @@
 #include <assert.h>
 
 int test_vychislen(char a[30]) {
-    assert(vychislen(a,5)==6);
+    assert(vychislen(a[4],5)==6);
 }
 
 #undef main
 int main () {
-    char a[30];
+    char a[30]={1,2,7,6,4,1,2,5};
     test_vychislen(a);
 }
  
